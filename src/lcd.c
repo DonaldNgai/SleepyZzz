15acdbac11e856caf3c6065241cb750ee7280803
Donald Ngai <dptngai@uwaterloo.ca>
2017-07-18 17:47:16 -0400

    Added functionality for LCD

--- src/lcd.c
+++ src/lcd.c
@@ -12,39 +12,93 @@
 #define LCD_i2c_address	0x28 //80
 #define LCD_max_data_rate 100 //KHz
 
-uint8_t txData[16];
-uint8_t rxData[16];
+uint8_t tx_packet[3];
+uint8_t rx_packet; //Unnecessary
+
+/*****************************************************************************
+ * Private functions
+ ****************************************************************************/
+
+static void send_packet(int number_of_bytes)
+{
+	SetupXferRecAndExecute(LCD_i2c_address, tx_packet, number_of_bytes, &rx_packet, 0);
+}
 
 /*****************************************************************************
  * Public functions
  ****************************************************************************/
-
-void clear_lcd()
+void turn_on_lcd()
 {
-
+	tx_packet[0] = 0xFE;
+	tx_packet[1] = 0x41;
+	send_packet(2);	
 }
 
 void turn_off_lcd()
 {
-	txData[0] = 0xFE; // Prefix
-	txData[1] = 0x53; // Command
-	txData[2] = 0x06; // Command
-//	SetupXferRecAndExecute(LCD_i2c_address, txData, 2, rxData, 0);
+	tx_packet[0] = 0xFE;
+	tx_packet[1] = 0x42;
+	send_packet(2);	
 }
 
-void LCD_print_string()
+void set_lcd_cursor(unsigned int x)
 {
-
-//	SetupXferRecAndExecute((uint16_t)LCD_i2c_address, txData, 5, rxData, 0);
-//	turn_off_lcd();uint16_t AddressI2C, bool ledStateOut, bool address10Bit
-//	sendI2CMaster(LCD_i2c_address, 0, 0);
-	txData[0] = 0xFE; // Prefix
-	txData[1] = 0x72; // Command
-//	txData[0] = 0xFE; // Prefix
-//		txData[1] = 0x53; // Command
-//		txData[2] = 0x06; // Command
-
-	SetupXferRecAndExecute(LCD_i2c_address, txData, 2, rxData, 0);
-
+	tx_packet[0] = 0xFE;
+	tx_packet[1] = 0x45;
+	tx_packet[2] = x;
+	send_packet(3);	
 }
+
+// void lcd_clear_line(unsigned int x)
+// {
+// 	unsigned int i;
+	
+// 	for (i = 0; i < x; i++) {
+// 		//Write blank character x times
+// 		tx_packet[i] = 0x20;
+// 	}
+// 	send_packet(x);
+// }
+
+void lcd_clear(void)
+{
+	tx_packet[0] = 0xFE;
+	tx_packet[1] = 0x51;
+	send_packet(2);
+}
+
+void LCP_print_char(char c)
+{
+	tx_packet[0] = (int) c;
+	send_packet(1);	
+}
+
+void LCD_print_string(int row, char* string)
+{
+	while (*string) {
+    	LCP_print_char(*string++);
+  	}
+}
+
+void show_lcd_i2c_address()
+{
+	tx_packet[0] = 0xFE;
+	tx_packet[1] = 0x72;
+	send_packet(2);	
+}
+
+//param brightness: must be between 1 and 8.
+void set_lcd_backlight_brightness(int brightness)
+{
+	//Make sure we have a valid brightness
+	if (brightness > 8 || brightness < 1)
+	{
+		return;
+	}
+	tx_packet[0] = 0xFE;
+	tx_packet[1] = 0x53;
+	tx_packet[2] = brightness;
+	send_packet(3);	
+}
+
 
