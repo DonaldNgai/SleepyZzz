a01c0698f798b73ffc7f56a39e3ff20b0239bfed
Donald Ngai <donaldptngai@gmail.com>
2017-07-27 18:37:41 -0400

    Added ability to input what to send to wifi module and also made it so that it sends \r\r\n\r\n

--- Debug/src/lcd.d
+++ Debug/src/lcd.d
@@ -1,129 +1,131 @@
 src/lcd.o src/lcd.d: ../src/lcd.c ../src/lcd.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_board_lpc812max/inc/board.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/chip.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/lpc_types.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/sys_config.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/cmsis.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/core_cm0plus.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/core_cmInstr.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/core_cmFunc.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/romapi_8xx.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/iap.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/error_8xx.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/rom_i2c_8xx.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/rom_pwr_8xx.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/rom_uart_8xx.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/syscon_8xx.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/clock_8xx.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/iocon_8xx.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/swm_8xx.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/fmc_8xx.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/pinint_8xx.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/pmu_8xx.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/acmp_8xx.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/crc_8xx.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/gpio_8xx.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/mrt_8xx.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/uart_8xx.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/ring_buffer.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/wkt_8xx.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/wwdt_8xx.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/sct_8xx.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/sct_pwm_8xx.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/spi_8xx.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/spi_common_8xx.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/i2cm_8xx.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/i2c_common_8xx.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/i2cs_8xx.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/spim_8xx.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/spis_8xx.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/irc_8xx.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_board_lpc812max/inc/board_api.h \
- /Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/lpc_types.h \
- ../src/i2c.h
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_board_lpc812max/inc/board.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/chip.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/lpc_types.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/sys_config.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/cmsis.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/core_cm0plus.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/core_cmInstr.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/core_cmFunc.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/romapi_8xx.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/iap.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/error_8xx.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/rom_i2c_8xx.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/rom_pwr_8xx.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/rom_uart_8xx.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/syscon_8xx.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/clock_8xx.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/iocon_8xx.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/swm_8xx.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/fmc_8xx.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/pinint_8xx.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/pmu_8xx.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/acmp_8xx.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/crc_8xx.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/gpio_8xx.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/mrt_8xx.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/uart_8xx.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/ring_buffer.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/wkt_8xx.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/wwdt_8xx.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/sct_8xx.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/sct_pwm_8xx.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/spi_8xx.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/spi_common_8xx.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/i2cm_8xx.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/i2c_common_8xx.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/i2cs_8xx.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/spim_8xx.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/spis_8xx.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/irc_8xx.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_board_lpc812max/inc/board_api.h \
+ /home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/lpc_types.h \
+ ../src/i2c.h ../src/helper.h
 
 ../src/lcd.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_board_lpc812max/inc/board.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_board_lpc812max/inc/board.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/chip.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/chip.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/lpc_types.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/lpc_types.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/sys_config.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/sys_config.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/cmsis.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/cmsis.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/core_cm0plus.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/core_cm0plus.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/core_cmInstr.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/core_cmInstr.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/core_cmFunc.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/core_cmFunc.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/romapi_8xx.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/romapi_8xx.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/iap.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/iap.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/error_8xx.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/error_8xx.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/rom_i2c_8xx.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/rom_i2c_8xx.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/rom_pwr_8xx.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/rom_pwr_8xx.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/rom_uart_8xx.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/rom_uart_8xx.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/syscon_8xx.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/syscon_8xx.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/clock_8xx.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/clock_8xx.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/iocon_8xx.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/iocon_8xx.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/swm_8xx.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/swm_8xx.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/fmc_8xx.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/fmc_8xx.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/pinint_8xx.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/pinint_8xx.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/pmu_8xx.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/pmu_8xx.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/acmp_8xx.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/acmp_8xx.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/crc_8xx.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/crc_8xx.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/gpio_8xx.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/gpio_8xx.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/mrt_8xx.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/mrt_8xx.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/uart_8xx.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/uart_8xx.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/ring_buffer.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/ring_buffer.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/wkt_8xx.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/wkt_8xx.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/wwdt_8xx.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/wwdt_8xx.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/sct_8xx.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/sct_8xx.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/sct_pwm_8xx.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/sct_pwm_8xx.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/spi_8xx.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/spi_8xx.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/spi_common_8xx.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/spi_common_8xx.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/i2cm_8xx.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/i2cm_8xx.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/i2c_common_8xx.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/i2c_common_8xx.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/i2cs_8xx.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/i2cs_8xx.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/spim_8xx.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/spim_8xx.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/spis_8xx.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/spis_8xx.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/irc_8xx.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/irc_8xx.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_board_lpc812max/inc/board_api.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_board_lpc812max/inc/board_api.h:
 
-/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc/lpc_types.h:
+/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc/lpc_types.h:
 
 ../src/i2c.h:
+
+../src/helper.h:
--- Debug/src/subdir.mk
+++ Debug/src/subdir.mk
@@ -12,8 +12,11 @@ C_SRCS += \
 ../src/i2c.c \
 ../src/lcd.c \
 ../src/mtb.c \
+../src/print.c \
 ../src/switch_matrix.c \
-../src/sysinit.c 
+../src/sysinit.c \
+../src/uart.c \
+../src/wifi.c 
 
 OBJS += \
 ./src/SleepyZzz.o \
@@ -24,8 +27,11 @@ OBJS += \
 ./src/i2c.o \
 ./src/lcd.o \
 ./src/mtb.o \
+./src/print.o \
 ./src/switch_matrix.o \
-./src/sysinit.o 
+./src/sysinit.o \
+./src/uart.o \
+./src/wifi.o 
 
 C_DEPS += \
 ./src/SleepyZzz.d \
@@ -36,15 +42,18 @@ C_DEPS += \
 ./src/i2c.d \
 ./src/lcd.d \
 ./src/mtb.d \
+./src/print.d \
 ./src/switch_matrix.d \
-./src/sysinit.d 
+./src/sysinit.d \
+./src/uart.d \
+./src/wifi.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU C Compiler'
-	arm-none-eabi-gcc -DDEBUG -D__CODE_RED -DCORE_M0PLUS -D__MTB_BUFFER_SIZE=256 -D__USE_LPCOPEN -D__LPC8XX__ -D__REDLIB__ -I"/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_board_lpc812max/inc" -I"/Users/adamseeto/Desktop/stuff/school/FYDP/code/lpc_chip_8xx/inc" -O0 -fno-common -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -mcpu=cortex-m0 -mthumb -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	arm-none-eabi-gcc -DDEBUG -D__CODE_RED -DCORE_M0PLUS -D__MTB_BUFFER_SIZE=256 -D__USE_LPCOPEN -D__LPC8XX__ -D__REDLIB__ -I"/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/SleepyZzz/inc" -I"/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_board_lpc812max/inc" -I"/home/donald/Documents/MCUXpresso_10.0.0_344/workspace/lpc_chip_8xx/inc" -O0 -fno-common -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -mcpu=cortex-m0 -mthumb -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
